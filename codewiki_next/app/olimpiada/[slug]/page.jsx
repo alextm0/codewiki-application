ba21f64dc1a25afe2c214d30da8745d19606926e
Toma Alexandru <alextoma1704@gmail.com>
2024-05-06 13:37:20 +0300

    [CWT-002] Update fetching

---
+++ codewiki_next/app/olimpiada/[slug]/page.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+import ResourcesTable from "@/components/EduResources";
+import PageDivider from "@/components/PageDivider";
+import ProblemSetTable from "@/components/ProblemList";
+
+const Page = () => {
+  const mockResources = [
+    {
+      source: "Resource A",
+      title: "Introduction to React",
+      link: "https://reactjs.org",
+      description: "A comprehensive guide to getting started with React.",
+    },
+    {
+      source: "Resource B",
+      title: "Advanced React Patterns",
+      link: "https://advancedreact.com",
+      description:
+        "Learn about advanced patterns in React for scaling your applications.",
+    },
+    {
+      source: "Resource C",
+      title: "React Testing",
+      link: "https://testing-library.com/react",
+      description: "Tools and techniques for testing React applications.",
+    },
+  ];
+
+  const mockProblemSet = [
+    {
+      source: "LeetCode",
+      name: "Two Sum",
+      sourceLink: "https://leetcode.com/problems/two-sum",
+      link: "https://leetcode.com/problems/two-sum",
+      badge: "easy",
+      tags: "Array, Hash Table",
+    },
+    {
+      source: "Codeforces",
+      name: "Graph Cutting",
+      sourceLink: "https://codeforces.com/problemset/problem/115/A",
+      link: "https://codeforces.com/problemset/problem/115/A",
+      badge: "hard",
+      tags: "Graphs, DFS",
+    },
+    {
+      source: "HackerRank",
+      name: "Subarray Division",
+      sourceLink: "https://www.hackerrank.com/challenges/the-birthday-bar",
+      link: "https://www.hackerrank.com/challenges/the-birthday-bar",
+      badge: "normal",
+      tags: "Search",
+    },
+  ];
+
+  return (
+    <div>
+      <div className="bg-gradient-to-br from-[#00044D] to-[#00044D] mb-16">
+        <PageDivider />
+      </div>
+      <div className="max-w-[1024px] mx-auto">
+        <div className="max-w-[1024px] mx-auto">
+          {
+            <ResourcesTable header="Resources" resource={mockResources} />
+          }
+
+          {
+            <ProblemSetTable
+              header="Practice Problems"
+              problemSet={mockProblemSet}
+            />
+          }
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
--- codewiki_strapi/types/generated/contentTypes.d.ts
+++ codewiki_strapi/types/generated/contentTypes.d.ts
@@ -362,71 +362,6 @@ export interface AdminTransferTokenPermission extends Schema.CollectionType {
   };
 }
 
-export interface ApiBlogBlog extends Schema.CollectionType {
-  collectionName: 'blogs';
-  info: {
-    singularName: 'blog';
-    pluralName: 'blogs';
-    displayName: 'Blog';
-    description: '';
-  };
-  options: {
-    draftAndPublish: true;
-  };
-  attributes: {
-    title: Attribute.String;
-    description: Attribute.Text;
-    slug: Attribute.UID<'api::blog.blog', 'title'>;
-    content: Attribute.RichText;
-    rating: Attribute.Integer;
-    badge: Attribute.String;
-    publishDate: Attribute.Date;
-    articleImage: Attribute.Media;
-    problemSet: Attribute.Blocks;
-    resources: Attribute.Blocks;
-    createdAt: Attribute.DateTime;
-    updatedAt: Attribute.DateTime;
-    publishedAt: Attribute.DateTime;
-    createdBy: Attribute.Relation<'api::blog.blog', 'oneToOne', 'admin::user'> &
-      Attribute.Private;
-    updatedBy: Attribute.Relation<'api::blog.blog', 'oneToOne', 'admin::user'> &
-      Attribute.Private;
-  };
-}
-
-export interface ApiCategoryCategory extends Schema.CollectionType {
-  collectionName: 'categories';
-  info: {
-    singularName: 'category';
-    pluralName: 'categories';
-    displayName: 'Category';
-  };
-  options: {
-    draftAndPublish: true;
-  };
-  attributes: {
-    category: Attribute.String;
-    name: Attribute.String;
-    description: Attribute.Text;
-    topics: Attribute.JSON;
-    createdAt: Attribute.DateTime;
-    updatedAt: Attribute.DateTime;
-    publishedAt: Attribute.DateTime;
-    createdBy: Attribute.Relation<
-      'api::category.category',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-    updatedBy: Attribute.Relation<
-      'api::category.category',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-  };
-}
-
 export interface PluginUploadFile extends Schema.CollectionType {
   collectionName: 'files';
   info: {
@@ -742,6 +677,71 @@ export interface PluginI18NLocale extends Schema.CollectionType {
   };
 }
 
+export interface ApiBlogBlog extends Schema.CollectionType {
+  collectionName: 'blogs';
+  info: {
+    singularName: 'blog';
+    pluralName: 'blogs';
+    displayName: 'Blog';
+    description: '';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    title: Attribute.String;
+    description: Attribute.Text;
+    slug: Attribute.UID<'api::blog.blog', 'title'>;
+    content: Attribute.RichText;
+    rating: Attribute.Integer;
+    badge: Attribute.String;
+    publishDate: Attribute.Date;
+    articleImage: Attribute.Media;
+    problemSet: Attribute.Blocks;
+    resources: Attribute.Blocks;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<'api::blog.blog', 'oneToOne', 'admin::user'> &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<'api::blog.blog', 'oneToOne', 'admin::user'> &
+      Attribute.Private;
+  };
+}
+
+export interface ApiCategoryCategory extends Schema.CollectionType {
+  collectionName: 'categories';
+  info: {
+    singularName: 'category';
+    pluralName: 'categories';
+    displayName: 'Category';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    category: Attribute.String;
+    name: Attribute.String;
+    description: Attribute.Text;
+    topics: Attribute.JSON;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::category.category',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::category.category',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 declare module '@strapi/types' {
   export module Shared {
     export interface ContentTypes {
@@ -752,14 +752,14 @@ declare module '@strapi/types' {
       'admin::api-token-permission': AdminApiTokenPermission;
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
-      'api::blog.blog': ApiBlogBlog;
-      'api::category.category': ApiCategoryCategory;
       'plugin::upload.file': PluginUploadFile;
       'plugin::upload.folder': PluginUploadFolder;
       'plugin::users-permissions.permission': PluginUsersPermissionsPermission;
       'plugin::users-permissions.role': PluginUsersPermissionsRole;
       'plugin::users-permissions.user': PluginUsersPermissionsUser;
       'plugin::i18n.locale': PluginI18NLocale;
+      'api::blog.blog': ApiBlogBlog;
+      'api::category.category': ApiCategoryCategory;
     }
   }
 }
