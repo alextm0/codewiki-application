df173dbe9a746a51a95b3d02031068419b6dee29
Toma Alexandru <alextoma1704@gmail.com>
2024-05-06 13:44:37 +0300

    [CWT-003] Test commit

--- codewiki_next/app/olimpiada/[slug]/page.jsx
+++ codewiki_next/app/olimpiada/[slug]/page.jsx
@@ -54,6 +54,33 @@ const Page = () => {
     },
   ];
 
+  const mockProblemSet2 = [
+    {
+      source: "LeetCode",
+      name: "Two Sum",
+      sourceLink: "https://leetcode.com/problems/two-sum",
+      link: "https://leetcode.com/problems/two-sum",
+      badge: "easy",
+      tags: "Array, Hash Table",
+    },
+    {
+      source: "Codeforces",
+      name: "Graph Cutting",
+      sourceLink: "https://codeforces.com/problemset/problem/115/A",
+      link: "https://codeforces.com/problemset/problem/115/A",
+      badge: "hard",
+      tags: "Graphs, DFS",
+    },
+    {
+      source: "HackerRank",
+      name: "Subarray Division",
+      sourceLink: "https://www.hackerrank.com/challenges/the-birthday-bar",
+      link: "https://www.hackerrank.com/challenges/the-birthday-bar",
+      badge: "normal",
+      tags: "Search",
+    },
+  ];
+
   return (
     <div>
       <div className="bg-gradient-to-br from-[#00044D] to-[#00044D] mb-16">
